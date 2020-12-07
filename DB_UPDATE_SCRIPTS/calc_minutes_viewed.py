62bacd56cd4ae0aade6f748becdf2a679e5f255c
Sean <mcmanus@tcd.ie>
2020-12-06 20:20:07 -0500

    minutes viewed aggregator update

--- DB_UPDATE_SCRIPTS/calc_minutes_viewed.py
+++ DB_UPDATE_SCRIPTS/calc_minutes_viewed.py
@@ -13,30 +13,7 @@ db_string = "postgres://postgres:PW@localhost:5432/twitch_project"
 db = create_engine(db_string, pool_pre_ping=True)
 session = sessionmaker(bind=db)()
 
-##insert the following information into a minutes_viewed table daily.
+##The query below inserts yesterday's data since this script is ran after midnight
 session.connection().connection.set_isolation_level(0)
-session.execute(query)
-session.connection().connection.set_isolation_level(1)  
-#minutes_viewed_table fields
-id serial PRIMARY KEY,game_id integer, game_name varchar, minutes_viewed integer,date date
-##this is all rough work - i can't write this until i have data in this db as i need to test it.
-INSERT INTO minutes_viewed
-(game_id,
- game_name,
- minutes_viewed,
- date)
-SELECT
-game_id,
-game_name,
-,viewers
-NOW()
-FROM views AS V1
-INNER JOIN views AS V2
-ON v1.game_id = V2.game_id
-##pseudo code - only want to select the values for that game for that day
-WHERE views.time BETWEEN 
-##pseudo code
-##all the viewers for a game in a day = x
-##x*10 = total minutes viewed per day = y
-##
-##store game, y, date
+session.execute('INSERT INTO minutes_viewed (game_id,game_name,minutes_viewed,date) SELECT game_id, game_name, SUM(viewers)*10, CURRENT_DATE FROM views WHERE time >= (CURRENT_DATE-1) AND time < (CURRENT_DATE) GROUP BY game_name,game_id ORDER BY sum(viewers) DESC')
+session.connection().connection.set_isolation_level(1) 
